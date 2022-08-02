53d5c014cdfe5443ebcfad1ffc7b16cda8f7693f
Vinicius Soares <viniciussoaresaqw@hotmail.com>
2022-08-02 09:27:33 -0300

    Finish

--- src/app/pages/addons/chart-cadastro/chart-cadastro.component.ts
+++ src/app/pages/addons/chart-cadastro/chart-cadastro.component.ts
@@ -1,28 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+
 
 @Component({
   selector: 'app-chart-cadastro',
   templateUrl: './chart-cadastro.component.html',
   styleUrls: ['./chart-cadastro.component.scss']
 })
-export class ChartCadastroComponent {
+export class ChartCadastroComponent implements OnInit{
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  @Input() saida?: number;
+  @Input() entrada?: number;
+
   public barChartLegend = false;
   public barChartPlugins = [];
 
   public barChartData: ChartConfiguration<'bar'>['data'] = {
+    labels: ['Entrada', 'Saída'],
     datasets: [
-      { data: [ 100], hoverBackgroundColor: 'white'},
-    ],
+      { data: [this.entrada || 0, this.saida || 0],
+        hoverBackgroundColor: 'rgba(0,140,0,0.3)',
+        backgroundColor: 'rgba(0,140,0,0.6)',
+        borderWidth: 2,
+        borderColor: 'rgba(0,140,0,0.6)',
+        hoverBorderColor: 'yellow',
+      }]
   };
-
-  public barChartLabels: Label[] = [];
 
   public barChartOptions: ChartOptions = {
     responsive: true,
-    backgroundColor: 'green',
-    
   };
 
+  ngOnInit(): void {
+    this.barChartData.datasets[0].data = [this.entrada || 0, this.saida || 0];
+    setInterval(() => { this.update(); }, 1000);
+  }
   
+  public update(): void {
+    this.barChartData.datasets[0].data = [this.entrada || 0, this.saida || 0];
+    this.chart?.update();
+  } 
 }
--- src/app/pages/components/cadastro-produto/cadastro-produto.component.ts
+++ src/app/pages/components/cadastro-produto/cadastro-produto.component.ts
@@ -1,16 +1,18 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
-import { Produto } from '../../interfaces/produto';
+import { Component, DoCheck } from '@angular/core';
+import { Produto } from 'src/app/interfaces/produto';
 
 @Component({
   selector: 'app-cadastro-produto',
   templateUrl: './cadastro-produto.component.html',
   styleUrls: ['./cadastro-produto.component.scss'],
 })
-export class CadastroProdutoComponent implements OnInit, DoCheck {
+export class CadastroProdutoComponent implements DoCheck {
+
+  public entrada: number = Number(localStorage.getItem('entrada') || '0');
+  public saida: number = Number(localStorage.getItem('saida') || '0');
   public list: Array<Produto> = JSON.parse(
     localStorage.getItem('list') || '[]'
   );
-
   public produto: Produto = {
     nome: '',
     preco: 0,
@@ -20,47 +22,55 @@ export class CadastroProdutoComponent implements OnInit, DoCheck {
     telefone: '',
     data: '',
   };
+  public types: Array<string> = [
+    'Comida',
+    'Acessorio',
+    'Roupa',
+    'Banho',
+    'Utencilio',
+    'Eletronico',
+    'Produto de beleza',
+    'Artigos de decoração'
+  ]
 
   constructor() {}
   ngDoCheck(): void {
     this.list = JSON.parse(localStorage.getItem('list') || '[]');
-    this.list.sort( (a, b) => { return b.quantidade - a.quantidade; });
+    this.entrada = Number(localStorage.getItem('entrada') || '0');
+    this.saida = Number(localStorage.getItem('saida') || '0');
+    this.list = this.list.sort((a, b) => {
+      return b.quantidade - a.quantidade;
+    });
   }
-
-  ngOnInit(): void {}
-
-
   public addProduct(): void {
-    this.list.push(this.produto);
-    localStorage.setItem('list', JSON.stringify(this.list));
+    if(
+      this.produto.data &&
+      this.produto.descricao &&
+      this.produto.preco &&
+      this.produto.nome &&
+      this.produto.telefone &&
+      this.produto.quantidade &&
+      this.produto.tipo
+    ){
+      this.list.push(this.produto);
+      localStorage.setItem('list', JSON.stringify(this.list));
+      localStorage.setItem('entrada', String(this.entrada+1));
+      this.produto.data = ''
+      this.produto.descricao = ''
+      this.produto.preco = 0
+      this.produto.nome = ''
+      this.produto.telefone = ''
+      this.produto.quantidade = 0
+      this.produto.tipo = ''
+    }
+    else{
+      window.alert('Digite todos os valores para poder salvar')
+    }
   }
   
   public sellProduct(index: number): void {
     this.list[index].quantidade -= 1;
     localStorage.setItem('list', JSON.stringify(this.list));
-  }
-
-  public sortList(event: string) {
-    if( event == "tipo"){
-      this.list = this.list.sort( (a, b) => {return a.nome.localeCompare(b.nome)});
-    }
-    if( event == "nome"){
-      this.list = this.list.sort( (a, b) => {return a.nome.localeCompare(b.nome)});
-    }
-    if( event == "preco"){
-      this.list = this.list.sort( (a, b) => {return a.preco - b.preco});
-    }
-    if( event == "descricao"){
-      this.list = this.list.sort( (a, b) => {return a.descricao.localeCompare(b.descricao)});
-    }
-    if( event == "quantidade"){
-      this.list = this.list.sort( (a, b) => {return a.quantidade - b.quantidade});
-    }
-    if( event == "telefone"){
-      this.list = this.list.sort( (a, b) => {return a.telefone.localeCompare(b.telefone)});
-    }
-    if( event == "data"){
-      this.list = this.list.sort( (a, b) => {return a.data.localeCompare(b.data)});
-    }
+    localStorage.setItem('saida', String(this.saida+1));
   }
 }
